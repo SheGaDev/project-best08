4bac86855061486ef3ca55ed22f7ce586692cd9f
boblmbq <sichkardanylo@gmail.com>
2023-08-20 04:35:11 +0200

    невелика поправка
    потрібно щоб ці імпорти були тут, для редрингу популярного та категорій

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,8 +1,6 @@
 import TastyTreatsAPI from '@api/TastyTreats';
 import { categoriesRendering } from './categoriesRendering';
 import { popularRendering } from './popular';
-
 export const API = new TastyTreatsAPI();
-
 categoriesRendering();
 popularRendering();

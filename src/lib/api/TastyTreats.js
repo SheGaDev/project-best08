f1cc98aee5f59d2d4849eb7ca1eced64a8acd8cd
Vadym Prokhorenko <prokhorenko.vadim.98@gmail.com>
2023-08-20 03:14:32 +0300

    Edit caching data.

--- src/lib/api/TastyTreats.js
+++ src/lib/api/TastyTreats.js
@@ -2,61 +2,63 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://tasty-treats-backend.p.goit.global/api/';
 
 export default class TastyTreatsAPI {
-  constructor() {
-    this.cache = {
-      categories: [],
-      areas: [],
-      ingredients: [],
-    };
-    this.currentPage = 1;
-  }
-  // { title, category, area, ingredients, time }
-  requestJoin(data) {
-    const query = ['limit=9'];
-    for (const [key, value] of Object.entries(data)) {
-      query.push(`${key}=${value}`);
+    constructor() {
+        this.cache = {
+            categories: [],
+            areas: [],
+            ingredients: [],
+        };
+        this.currentPage = 1;
     }
-    return query.join('&');
-  }
-  async fetchRecipes(data = {}) {
-    const query = Object.keys(data).length ? `recipes?${this.requestJoin(data)}` : 'recipes?limit=9';
-    const response = await axios(query);
-    return response.data;
-  }
-  async fetchRecipe(recipeId) {
-    const response = await axios(`recipes/${recipeId}`);
-    return response.data;
-  }
-  async fetchEvents() {
-    const response = await axios('events');
-    return response.data;
-  }
-  async fetchCategories() {
-    const response = await axios('categories');
-    this.cache.categories = [...response.data];
-    return response.data;
-  }
-  async fetchAreas() {
-    const response = await axios('areas');
-    this.cache.areas = [...response.data];
-    return response.data;
-  }
-  async fetchIngredients() {
-    const response = await axios('ingredients');
-    this.cache.ingredients = [...response.data];
-    return response.data;
-  }
-  async getPopularRecipes() {
-    const response = await axios('recipes/popular');
-    return response.data;
-  }
-  get getCategories() {
-    return this.cache.categories;
-  }
-  get getAreas() {
-    return this.cache.areas;
-  }
-  get getIngredients() {
-    return this.cache.ingredients;
-  }
+    // { title, category, area, ingredients, time }
+    requestJoin(data) {
+        let query = "limit=9";
+        for (const [key, value] of Object.entries(data)) {
+            query += `&${key}=${value}`;
+        }
+        return query.join('&');
+    }
+    async fetchRecipes(data = {}) {
+        const response = await axios(`recipes?${this.requestJoin(data)}`);
+        return response.data;
+    }
+    async fetchRecipe(recipeId) {
+        const response = await axios(`recipes/${recipeId}`);
+        return response.data;
+    }
+    async fetchEvents() {
+        const response = await axios('events');
+        return response.data;
+    }
+    async fetchCategories() {
+        if (this.cache.categories.length) return this.cache.categories;
+        const response = await axios('categories');
+        this.cache.categories = [...response.data];
+        return response.data;
+    }
+    async fetchAreas() {
+        if (this.cache.areas.length) return this.cache.areas;
+        const response = await axios('areas');
+        this.cache.areas = [...response.data];
+        return response.data;
+    }
+    async fetchIngredients() {
+        if (this.cache.ingredients.length) return this.cache.ingredients;
+        const response = await axios('ingredients');
+        this.cache.ingredients = [...response.data];
+        return response.data;
+    }
+    async getPopularRecipes() {
+        const response = await axios('recipes/popular');
+        return response.data;
+    }
+    get getCategories() {
+        return this.cache.categories;
+    }
+    get getAreas() {
+        return this.cache.areas;
+    }
+    get getIngredients() {
+        return this.cache.ingredients;
+    }
 }

b78c2f6e6f04735136560fa30b9a8c318b0c3086
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-08-20 16:58:03 +0200

    add current-mob-highlight

--- src/scripts/current-pg.js
+++ src/scripts/current-pg.js
@@ -1,14 +1,15 @@
 const refs = {
     currentUrl: window.location.href,
-    link: document.querySelectorAll('.header-list a'),
+    links: document.querySelectorAll('.header-list a, .mobile-menu-list a'),
     currentHome: document.querySelector('#home-link'),
+    currentMobileHome: document.querySelector('#home-link-mobile'),
   };
 
-refs.link.forEach(function (link) {
-if (link.href === refs.currentUrl) {
-link.classList.add('current');
-} 
-else {
+  
+  refs.links.forEach(function (link) {
+    if (link.href === refs.currentUrl) {
+      link.classList.add('current');
+    } else {
       link.classList.remove('current');
     }
   });

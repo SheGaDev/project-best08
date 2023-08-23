cd1dd0581ec510b86dbf79475a32ea89ad6685f5
boblmbq <sichkardanylo@gmail.com>
2023-08-23 17:20:33 +0200

    most work done

--- src/lib/api/refs.js
+++ src/lib/api/refs.js
@@ -1,4 +1,7 @@
 export const refs = {
   categoriesEl: document.querySelector('.categories-list'),
   popularEl: document.querySelector('.popular-list'),
+  modal: document.querySelector('[data-modal]'),
+  openModalBtn: document.querySelector('.btn-open-modal'),
+  closeModalBtn: document.querySelector('.close-modal'),
 };
--- src/scripts/order-now.js
+++ src/scripts/order-now.js
@@ -1,14 +1,12 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+import { refs } from '@/lib/api/refs';
+console.log(refs.modal);
+console.log(refs.openModalBtn);
+console.log(refs.closeModalBtn);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+  
+}
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
